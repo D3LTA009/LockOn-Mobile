61a076c90894f80c548de1e41d3f07bb1c9d43f2
D3LTA009 <cauaisaac2012@gmail.com>
2026-01-31 13:52:55 -0300

    Implement loader with notification and link copy
    Add notification and clipboard functionality for script link.

--- loader.lua
+++ loader.lua
@@ -1,1 +1,13 @@
+-- LOADER PUBLICO
 
+local linkvertise = "https://link-hub.net/3053424/bnME7R5BTulk"
+
+setclipboard(linkvertise)
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Lock On Script",
+    Text = "Link copiado! Complete o Linkvertise para pegar o script.",
+    Duration = 6
+})
+
+warn("Complete o Linkvertise para obter o script.")
